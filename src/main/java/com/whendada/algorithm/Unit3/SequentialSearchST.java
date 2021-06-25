8fb9bb22e4e3c980cc7326ab9a32522a6b990c5e
wenkj <Dailian1234*+>
2021-06-25 09:48:20 +0800

    顺序查找

---
+++ src/main/java/com/whendada/algorithm/Unit3/SequentialSearchST.java
@@ -0,0 +1,54 @@
+package com.whendada.algorithm.Unit3;
+
+/**
+ * 顺序查找（基于无序链表）
+ */
+public class SequentialSearchST<Key, Value> {
+
+    private Node first;
+
+    private class Node {
+
+        Key key;
+
+        Value val;
+
+        Node next;
+
+        public Node(Key key, Value val, Node next) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public Value get(Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                return x.val;
+            }
+        }
+        return null;
+    }
+
+    public void put(Key key, Value val) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+        first = new Node(key, val, first);
+    }
+
+    public int size() {
+        int count = 0;
+        if (first == null) {
+            return 0;
+        }
+        for (Node x = first; x != null; x = x.next) {
+            count++;
+        }
+        return count;
+    }
+}
